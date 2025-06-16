b57fc03a1f2b4955fbb469ea124781bd429cf4fd
nunespetz <nunespetra380@gmail.com>
2025-06-16 22:20:13 +0100

    Update SignUpPage.jsx

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -146,7 +146,7 @@ const SignUpPage = () => {
 
       <AuthImagePattern
         title="Join our community"
-        subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+        subtitle="Connect with Fintecs Pro Team."
       />
     </div>
   );
